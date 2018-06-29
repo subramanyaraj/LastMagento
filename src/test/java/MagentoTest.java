fe4411352fc785b0a41dc19a007d99a8ff24c395
subramanyaraj <subramanyaraj87@gmail.com>
2018-06-29 09:49:32 +0530

    second commit

--- src/test/java/MagentoTest.java
+++ src/test/java/MagentoTest.java
@@ -4,15 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 public class MagentoTest {	
 	@Test
 	public void testRun()
 	{
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://magento.com");
+		driver.get("https://www.magento.com");
 		System.out.println(driver);
 		//home page
 		HomePage hp=new HomePage(driver);
